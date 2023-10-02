59138c840501b04fd6bc3663c07483a44ce2a081
Sean Breckenridge <seanbrecke@gmail.com>
2023-10-02 12:11:25 -0700

    move config to setup.cfg

--- setup.py
+++ setup.py
@@ -1,40 +1,4 @@
-import io
-from setuptools import setup, find_packages
-from pathlib import Path
+from setuptools import setup
 
-reqs = Path("requirements.txt").read_text().splitlines()
-
-
-# Use the README.md content for the long description:
-with io.open("README.md", encoding="utf-8") as fo:
-    long_description = fo.read()
-
-setup(
-    name="calcurse_load",
-    version="0.1.0",
-    url="https://github.com/seanbreckenridge/calcurse_load",
-    author="Sean Breckenridge",
-    author_email="seanbrecke@gmail.com",
-    description="""Sources events for calcurse from Google Calendar and todo.txt""",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="MIT",
-    packages=find_packages(),
-    test_suite="tests",
-    install_requires=reqs,
-    python_requires=">=3.7",
-    keywords="calendar todo",
-    entry_points={
-        "console_scripts": [
-            "calcurse_load = calcurse_load.__main__:cli",
-            "gcal_index = gcal_index.__main__:main",
-        ]
-    },
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-    ],
-)
+if __name__ == "__main__":
+    setup()
